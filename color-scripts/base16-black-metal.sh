58b781f7c5b8f5229299ab0efc7ec0dd883431b4
John McKnight <jmcknight@gmail.com>
2018-11-20 23:27:28 -0500

    Initial commit

---
+++ color-scripts/base16-black-metal.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# Adapted from Base16-Builder's Gnome Terminal template.
+
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="Base 16 Black Metal"
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="base-16-black-metal"
+[[ -z "$DCONFTOOL" ]] && DCONFTOOL=dconf
+[[ -z "$BASE_KEY" ]] && BASE_KEY=/org/mate/terminal/profiles
+
+PROFILE_KEY="$BASE_KEY/$PROFILE_SLUG"
+
+dset() {
+  local key="$1"; shift
+  local val="$1"; shift
+
+  "$DCONFTOOL" write "$PROFILE_KEY/$key" "$val"
+}
+
+# Because gconftool doesn't have "append"
+glist_append() {
+  local key="$1"; shift
+  local val="$1"; shift
+
+  local entries="$(
+    {
+      "$DCONFTOOL" read "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+      echo "'$val'"
+    } | head -c-1 | tr "\n" ,
+  )"
+
+  "$DCONFTOOL" write "$key" "[$entries]"
+}
+
+# Append the Base16 profile to the profile list
+glist_append /org/mate/terminal/global/profile-list "$PROFILE_SLUG"
+
+dset visible-name "'$PROFILE_NAME'"
+dset palette "'#000000:#5f8787:#dd9999:#a06666:#888888:#999999:#aaaaaa:#c1c1c1:#333333:#5f8787:#dd9999:#a06666:#888888:#999999:#aaaaaa:#c1c1c1'"
+dset background-color "'#000000'"
+dset foreground-color "'#c1c1c1'"
+dset bold-color "'#c1c1c1'"
+dset bold-color-same-as-fg "true"
+dset use-theme-colors "false"
+dset use-theme-background "false"
+
